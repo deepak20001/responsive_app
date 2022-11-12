6ba9070aa6e3675a194b57a5453a6db635236b09
Deepak <djamwal10@gmail.com>
2022-11-12 11:48:24 +0530

    main screen ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_app/const/poke_type_container..dart';
+import './const/text_style.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,13 +29,81 @@ class Demo extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Column(
-            children: const [
-              SizedBox(height: 30),
-              Text(
+            children: [
+              const SizedBox(height: 30),
+              const Text(
                 "PokeApp",
                 style: TextStyle(fontFamily: "circula_bold", fontSize: 36),
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
+              Expanded(
+                child: GridView.builder(
+                  //added bouncingeffect on scroll
+                  physics: const BouncingScrollPhysics(),
+                  //-------------------------------------
+                  shrinkWrap: true,
+                  itemCount: 10,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            bottom: -20,
+                            right: -20,
+                            child: Image.asset(
+                              "assets/pokeball.png",
+                              width: 150,
+                              color: Colors.white.withOpacity(0.5),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 8,
+                            right: 8,
+                            child: Image.asset(
+                              "assets/img1.png",
+                              width: 150,
+                            ),
+                          ),
+                          Positioned(
+                            top: 20,
+                            left: 10,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                headingText(
+                                  color: Colors.white,
+                                  size: 24,
+                                  text: "Bulbasaur",
+                                ),
+                                typeContainer(
+                                  color: Colors.white,
+                                  size: 16,
+                                  text: "Grass",
+                                ),
+                                const SizedBox(height: 6),
+                                typeContainer(
+                                  color: Colors.white,
+                                  size: 16,
+                                  text: "Poison",
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
