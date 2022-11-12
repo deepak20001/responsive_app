cfc0a9b03bdf729b9281e814886affb64a6e7854
Deepak <djamwal10@gmail.com>
2022-11-12 12:40:34 +0530

    ui created for details screen

---
+++ lib/details_screen.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app/const/poke_type_container..dart';
+import 'package:responsive_app/const/text_style.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          ),
+        ),
+        backgroundColor: Colors.red,
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    headingText(
+                        color: Colors.white, size: 36, text: "Bulbasaur"),
+                    Row(
+                      children: [
+                        typeContainer(
+                          color: Colors.white,
+                          size: 16,
+                          text: "Grass",
+                        ),
+                        const SizedBox(width: 6),
+                        typeContainer(
+                          color: Colors.white,
+                          size: 16,
+                          text: "Grass",
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 50),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          Image.asset(
+                            "assets/pokeball.png",
+                            width: 200,
+                            color: Colors.white.withOpacity(0.5),
+                          ),
+                          Image.asset(
+                            "assets/img1.png",
+                            width: 180,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(12),
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  children: [
+                    const TabBar(
+                      labelColor: Colors.black,
+                      unselectedLabelColor: Colors.grey,
+                      tabs: [
+                        Tab(text: "About"),
+                        Tab(text: "Base Stats"),
+                      ],
+                    ),
+                    Expanded(
+                      child: TabBarView(children: [
+                        Container(
+                          color: Colors.amber,
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 10),
+                              normalText(
+                                  color: Colors.black,
+                                  size: 16,
+                                  text:
+                                      "It can be  seen napping in the jungle having a seed on its back."),
+                              const SizedBox(height: 10),
+                              Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Column(
+                                        children: [
+                                          headingText(
+                                              color: Colors.grey,
+                                              size: 16,
+                                              text: "Height"),
+                                          const SizedBox(height: 6),
+                                          headingText(
+                                              color: Colors.grey,
+                                              size: 16,
+                                              text: "15ft"),
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          headingText(
+                                              color: Colors.grey,
+                                              size: 16,
+                                              text: "Weight"),
+                                          const SizedBox(height: 6),
+                                          headingText(
+                                              color: Colors.grey,
+                                              size: 16,
+                                              text: "15lbs"),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(color: Colors.cyan),
+                      ]),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_app/const/poke_type_container..dart';
+import 'package:responsive_app/details_screen.dart';
 import './const/text_style.dart';
 
 void main() {
@@ -49,56 +50,66 @@ class Demo extends StatelessWidget {
                     crossAxisSpacing: 8,
                   ),
                   itemBuilder: (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            bottom: -20,
-                            right: -20,
-                            child: Image.asset(
-                              "assets/pokeball.png",
-                              width: 150,
-                              color: Colors.white.withOpacity(0.5),
-                            ),
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const DetailsScreen(),
                           ),
-                          Positioned(
-                            bottom: 8,
-                            right: 8,
-                            child: Image.asset(
-                              "assets/img1.png",
-                              width: 150,
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              bottom: -20,
+                              right: -20,
+                              child: Image.asset(
+                                "assets/pokeball.png",
+                                width: 150,
+                                color: Colors.white.withOpacity(0.5),
+                              ),
                             ),
-                          ),
-                          Positioned(
-                            top: 20,
-                            left: 10,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                headingText(
-                                  color: Colors.white,
-                                  size: 24,
-                                  text: "Bulbasaur",
-                                ),
-                                typeContainer(
-                                  color: Colors.white,
-                                  size: 16,
-                                  text: "Grass",
-                                ),
-                                const SizedBox(height: 6),
-                                typeContainer(
-                                  color: Colors.white,
-                                  size: 16,
-                                  text: "Poison",
-                                ),
-                              ],
+                            Positioned(
+                              bottom: 8,
+                              right: 8,
+                              child: Image.asset(
+                                "assets/img1.png",
+                                width: 150,
+                              ),
                             ),
-                          ),
-                        ],
+                            Positioned(
+                              top: 20,
+                              left: 10,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  headingText(
+                                    color: Colors.white,
+                                    size: 24,
+                                    text: "Bulbasaur",
+                                  ),
+                                  typeContainer(
+                                    color: Colors.white,
+                                    size: 16,
+                                    text: "Grass",
+                                  ),
+                                  const SizedBox(height: 6),
+                                  typeContainer(
+                                    color: Colors.white,
+                                    size: 16,
+                                    text: "Poison",
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
